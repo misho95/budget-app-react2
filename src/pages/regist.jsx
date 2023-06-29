0e1e69924cc9bea637a191baaae9e95388fed04d
misho95 <chapidze.misho95@gmail.com>
2023-06-30 01:25:47 +0400

    Update regist.jsx

--- src/pages/regist.jsx
+++ src/pages/regist.jsx
@@ -1,3 +1,7 @@
+import { useState, useEffect } from "react";
+import { v4 as uuidv4 } from 'uuid';
+
+
 function Regist(){
 
     const localUserData = JSON.parse(localStorage.getItem('userDataBase'));
@@ -27,14 +31,12 @@ function Regist(){
         localStorage.setItem('userDataBase', JSON.stringify(userData));
     }, [userData])
 
+
+
     return(
         <div className="flex flex-col justify-center items-center">
             <h1 className="text-gray-800 dark:text-gray-200 text-xl">Registration</h1>
             <form className="w-full sm:w-1/3 flex flex-col gap-2">
-                <fieldset className="border-2 border-gray-300 dark:border-gray-600 p-2">
-                    <legend className="px-2 text-gray-500">UserName</legend>
-                    <input value={user} onChange={(e)=> { setUser(e.target.value)}} type="text" placeholder="username" className="bg-gray-200 dark:bg-gray-800 w-full py-1 px-2 focus:outline-none text-gray-800 dark:text-gray-200" required/>
-                </fieldset>
                 <fieldset className="border-2 border-gray-300 dark:border-gray-600 p-2">
                     <legend className="px-2 text-gray-500">Email</legend>
                     <input value={email} onChange={(e) => { setEmail(e.target.value)}} type="email" placeholder="example@mail" className="bg-gray-200 dark:bg-gray-800 w-full py-1 px-2 focus:outline-none text-gray-800 dark:text-gray-200" required/>
