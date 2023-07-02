d8c4dfbf08ef0774f1bacfe37a8ba4ef514e7814
Misho <chapidze.misho95@gmail.com>
2023-07-02 18:15:40 +0400

    final

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'bpg' : 'bpg_extrasquare'
+      }
+    },
   },
   plugins: [],
 }
