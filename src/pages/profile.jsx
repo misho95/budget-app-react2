ff1790463cd3b6115362d281ded21907bbaf8735
misho95 <chapidze.misho95@gmail.com>
2023-07-04 19:32:37 +0400

    final

--- src/pages/profile.jsx
+++ src/pages/profile.jsx
@@ -1,9 +1,11 @@
 import { useSearchParams } from "react-router-dom";
 import { useState } from "react";
 import Button from '../components/button'
+import { useNavigate } from "react-router-dom";
 
 function Profile(){
 
+    const navigate = useNavigate();
     const [searchParams, setSearchParams] = useSearchParams();
     const ID = searchParams.get('id');
     const getUserData = JSON.parse(localStorage.getItem('userDataBase'));
@@ -33,6 +35,17 @@ function Profile(){
     setEditImg(false);
     }
 
+    const deleteUser = () => {
+        const deleteUsr = getUserData.filter( (user) => {
+            if(user.id !== ID) return user;
+        });
+        localStorage.setItem('userDataBase', JSON.stringify(deleteUsr));
+        localStorage.removeItem('ID');
+        setTimeout( () => {
+            navigate('/')
+        }, 200)
+    }
+
     return(
         <div className="flex justify-center pt-10">
             <div className="w-1/2 border-2 border-gray-300 dark:border-gray-700 p-5 flex flex-col gap-5">
@@ -54,7 +67,7 @@ function Profile(){
                     <Button name={'შეცვლა'} handler={changeLink} />
                 </div>
                 }
-                
+                <Button name={'მომხმარებლის წაშლა'} handler={deleteUser}/>
             </div>
         </div>
     )
